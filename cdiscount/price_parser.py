e3be97ce21407b0ae8519a2b0ac38966a094421e
Alex Dean <alex.dean9g@gmail.com>
2020-04-26 18:53:50 +0200

    adding unit tests

--- cdiscount/price_parser.py
+++ cdiscount/price_parser.py
@@ -4,11 +4,14 @@ import re
 
 
 def parse_price(sku):
-    url = 'https://www.cdiscount.com/f-0-'
-    skuurl = url + sku + '.html'
-    req = urllib.request.Request(skuurl)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
-    getprice = re.findall(r'"price":(.*?),', str(respData))
-    price = float(getprice[0])
-    print(price)
+    if isinstance(sku, str):
+        url = 'https://www.cdiscount.com/f-0-'
+        skuurl = url + sku + '.html'
+        req = urllib.request.Request(skuurl)
+        resp = urllib.request.urlopen(req)
+        respData = resp.read()
+        getprice = re.findall(r'"price":(.*?),', str(respData))
+        price = float(getprice[0])
+        return price
+    else:
+        return False
